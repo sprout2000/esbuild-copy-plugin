9b64cb7d09d795c15282a3c3d3b17e589b756970
sprout2000 <wassabie64@gmail.com>
2022-08-20 14:22:56 +0900

    v1.0.0

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -5,6 +5,9 @@ interface CopyPluginOptions extends fs.CopySyncOptions {
     src?: string;
     dest?: string;
 }
+export declare const getDigest: (string: string) => string;
+export declare const getFileDigest: (path: string) => string | null;
+export declare const filter: (src: string, dest: string) => boolean;
 export declare const copyPlugin: (options?: CopyPluginOptions) => Plugin;
 export {};
 //# sourceMappingURL=index.d.ts.map
