3023cecfd20e53c800e1fd1d67b775080df33113
sprout2000 <wassabie64@gmail.com>
2022-08-20 14:17:40 +0900

    chore: Created jest.config.ts.

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  collectCoverage: true,
+  coverageReporters: ['text', 'json-summary'],
+};
+
+export default config;
