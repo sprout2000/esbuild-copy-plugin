cb4fe95602edf0be1ac48b1af3d73fb99b98134b
sprout2000 <wassabie64@gmail.com>
2022-09-25 07:24:53 +0900

    feat: Updated CopyPluginOptions.

--- src/index.test.ts
+++ src/index.test.ts
@@ -39,8 +39,8 @@ describe('Test Suites', () => {
     await build({
       plugins: [
         copyPlugin({
-          src: path.resolve(__dirname, './index.ts'),
-          dest: path.join(tmpDir, 'index.ts'),
+          from: path.resolve(__dirname, './index.ts'),
+          to: path.join(tmpDir, 'index.ts'),
         }),
       ],
     });
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import crypto from 'node:crypto';
 import { Plugin, PluginBuild } from 'esbuild';
 
 interface CopyPluginOptions extends fs.CopySyncOptions {
-  src?: string;
-  dest?: string;
+  from?: string;
+  to?: string;
 }
 
 export const getDigest = (string: string) => {
@@ -32,8 +32,8 @@ export const filter = (src: string, dest: string) => {
 export const copyPlugin = (options: CopyPluginOptions = {}): Plugin => ({
   name: 'esbuild-copy-plugin',
   setup(build: PluginBuild) {
-    const src = options.src || './assets';
-    const dest = options.dest || './dist';
+    const src = options.from || './assets';
+    const dest = options.to || './dist';
 
     build.onEnd(() =>
       // https://nodejs.org/api/fs.html#fscpsyncsrc-dest-options
